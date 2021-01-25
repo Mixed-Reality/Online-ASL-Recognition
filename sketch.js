fa624de1716493dc25ae4f292923ef1537327c30
Arjun <arj.kashyap123@gmail.com>
2021-01-25 19:21:39 +0530

    Data collection setup

--- sketch.js
+++ sketch.js
@@ -4,15 +4,39 @@ let pose = [];
 let predictions = [];
 let nn;
 let currentState = "waiting";
-let targetLabel;
+let targetLabel = null;
+
+let collectingData = false;
 
 // number of hand features detected = 21
 
+function isvalidKey(pressedKey) {
+  if (pressedKey.toLowerCase() >= "a" && pressedKey.toLowerCase() <= "z")
+    return true;
+
+  return false;
+}
+
+// check for key presses
 function keyPressed() {
-  targetLabel = key;
-  console.log(targetLabel);
-  console.log("Collecting");
-  currentState = "collecting";
+  // targetLabel = key;
+  pressedKey = key;
+  targetLabel = isvalidKey(pressedKey) ? pressedKey : null;
+  // save collected data if 's' is pressed
+  if (targetLabel.toLowerCase() === "s") {
+    nn.saveData();
+  } else if (targetLabel) {
+    console.log("Collecting");
+    console.log(targetLabel);
+    currentState = "collecting";
+
+    // Stop collecting after 5 seconds
+    setTimeout(() => {
+      console.log("Stopped Collecting");
+      currentState = "waiting";
+      targetLabel = null;
+    }, 10000);
+  }
 }
 
 function modelLoaded() {
@@ -30,7 +54,8 @@ function setup() {
   // handPoseNet.on("predict", gotHandPoses);
   handPoseNet.on("predict", (results) => {
     predictions = results;
-    gotHandPoses;
+    // console.log(results);
+    gotHandPoses(results);
   });
 
   video.hide();
@@ -45,11 +70,25 @@ function setup() {
   nn = ml5.neuralNetwork(options); // initialize dense neural network
 }
 
-function gotHandPoses(poses) {
-  pose = poses;
+function gotHandPoses(result) {
+  if (result.length > 0) {
+    pose = result[0].landmarks; // co-ordinates of the features
 
-  if (poses.length > 0) {
-    pose = poses[0].landmarks; // an array of xyz co-ordinates of hand
+    if (currentState == "collecting") {
+      let inputs = []; // inputs for neural network
+
+      for (let i = 0; i < pose.length; i++) {
+        // Storing feature co-ordinates in 1-D array
+        const x = parseInt(pose[i][0].toFixed(2));
+        const y = parseInt(pose[i][1].toFixed(2));
+        const z = parseInt(pose[i][2].toFixed(2));
+        inputs.push(x);
+        inputs.push(y);
+        inputs.push(z);
+      }
+      let target = [targetLabel];
+      nn.addData(inputs, target); // Add the data to neural network raw array
+    }
   }
 }
 
